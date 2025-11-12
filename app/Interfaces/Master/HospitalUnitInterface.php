272d8c7ea2277f8a21ed6ffb475b5efdaccf50b4
owlprojection <bagus.yanuar613@gmail.com>
2025-11-12 15:20:02 +0700

    feat: schemas (hospital units)

--- app/Interfaces/Master/HospitalUnitInterface.php
+++ app/Interfaces/Master/HospitalUnitInterface.php
@@ -2,7 +2,10 @@
 
 namespace App\Interfaces\Master;
 
+use App\Commons\Libs\Http\ServiceResponse;
+use App\Schemas\Master\HospitalInstallation\HospitalInstallationQuery;
+
 interface HospitalUnitInterface
 {
-    //
+    public function findAll(HospitalInstallationQuery $queryParams): ServiceResponse;
 }
--- app/Schemas/Master/HospitalUnit/HospitalUnitSchema.php
+++ app/Schemas/Master/HospitalUnit/HospitalUnitSchema.php
@@ -2,13 +2,116 @@
 
 namespace App\Schemas\Master\HospitalUnit;
 
-class HospitalUnitSchema
+use App\Commons\Libs\Http\BaseSchema;
+
+class HospitalUnitSchema extends BaseSchema
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    private $hospitalUnitId;
+    private $code;
+    private $name;
+    private $description;
+
+    protected function rules()
     {
-        //
+        return [
+            'hospital_unit_id' => 'required|uuid',
+            'code' => 'required|string',
+            'name' => 'required|string',
+            'description' => 'string'
+        ];
+    }
+
+    public function hydrateBody()
+    {
+        $code = $this->body['code'];
+        $name = $this->body['name'];
+        $hospitalUnitId = $this->body['hospital_unit_id'];
+        $description = !empty(trim($this->body['description'] ?? '')) ? $this->body['description'] : null;
+        $this->setName($name)
+            ->setCode($code)
+            ->setHospitalUnitId($hospitalUnitId)
+            ->setDescription($description);
+    }
+
+
+
+    /**
+     * Get the value of code
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set the value of code
+     *
+     * @return  self
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @return  self
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hospitalUnitId
+     */
+    public function getHospitalUnitId()
+    {
+        return $this->hospitalUnitId;
+    }
+
+    /**
+     * Set the value of hospitalUnitId
+     *
+     * @return  self
+     */
+    public function setHospitalUnitId($hospitalUnitId)
+    {
+        $this->hospitalUnitId = $hospitalUnitId;
+
+        return $this;
     }
 }
